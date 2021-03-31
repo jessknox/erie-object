004ba25069bc9529ea0ceac810ba5b6d206d60dd
jessknox <69935518+jessknox@users.noreply.github.com>
2021-03-31 22:37:38 +1300

    Delete frames directory

--- frames/assets/js/default.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  $('.frame-draggy').draggable();
-
-  $('body').on('click', '#toggle-overlay', function(e) {
-    e.preventDefault();
-  $(".index-container").toggleClass("index-container--open");
-  });
-});
